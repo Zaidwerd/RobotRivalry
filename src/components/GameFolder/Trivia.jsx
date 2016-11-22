9314b3ad40daba88a5e2e1a331d6ff5f86c52cda
Debika Sarkar <debikasarkar713@gmail.com>
2016-11-22 13:48:33 -0500

    changed login folder name

--- src/components/GameFolder/Trivia.jsx
+++ src/components/GameFolder/Trivia.jsx
@@ -1,1 +1,11 @@
 import React, { Component } from 'react';
+import style from './Trivia.css';
+
+class Trivia extends Component {
+  render(){
+    return (
+      <div></div>
+      );
+  }
+}
+export default Trivia;
