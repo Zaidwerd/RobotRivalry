79d05adacfb9c0368c58fd30a520638dde329715
Debika Sarkar <debikasarkar713@gmail.com>
2016-11-22 15:10:42 -0500

    file structuring

---
+++ src/StartPage/StartPage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import style from './StartPage.css';
+
+class StartPage extends Component {
+  render(){
+    return (
+
+      <div class="robotrivalry">Robot Rivalry</div>
+      <div class="cookingmama">cookingmama coming soon</div>
+      <div class="cookingmama">cookingmama coming soon</div>
+      );
+  }
+}
+export default StartPage;
