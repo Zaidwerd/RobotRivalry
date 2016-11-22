73d48056334c839d94564a29041bee9c1f265a27
grace moon <hi.gracemoon@gmail.com>
2016-11-22 17:13:21 -0500

    starting cat css

--- src/components/Categories/categories.jsx
+++ src/components/Categories/categories.jsx
@@ -8,15 +8,17 @@ class Categories extends Component {
   render(){
     return (
       <div id='container'>
-        <div className='category-box1'>category name</div>
-        <div className='category-box2'>category name</div>
-        <div className='category-box3'>category name</div>
-        <div className='category-box4'>category name</div>
-        <div className='category-box5'>category name</div>
-		    <div className='category-box6'>category name</div>
-        <div className='category-box7'>category name</div>
-        <div className='category-box8'>category name</div> 
-        <div className='category-box9'>category name</div>       
+      <div className='main-box'>
+        <button className='category-box1 box'>category name</div>
+        <button className='category-box2 box'>category name</div>
+        <button className='category-box3 box'>category name</div>
+        <button className='category-box4 box'>category name</div>
+        <button className='category-box5 box'>category name</div>
+		    <button className='category-box6 box'>category name</div>
+        <button className='category-box7 box'>category name</div>
+        <button className='category-box8 box'>category name</div> 
+        <button className='category-box9 box'>category name</div>       
+      </div>
       </div>
     );
   }
