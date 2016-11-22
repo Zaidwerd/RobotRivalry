b61e80447c3abf61a5c937ce6fdcfd926649095b
grace moon <hi.gracemoon@gmail.com>
2016-11-22 18:55:44 -0500

    pull

--- src/components/App/Game/Categories/categories.jsx
+++ src/components/App/Game/Categories/categories.jsx
@@ -10,14 +10,7 @@ class Categories extends Component {
       <div id='container'>
       <div className='main-box'>
         <button className='category-box1 box'>category name</div>
-        <button className='category-box2 box'>category name</div>
-        <button className='category-box3 box'>category name</div>
-        <button className='category-box4 box'>category name</div>
-        <button className='category-box5 box'>category name</div>
-		    <button className='category-box6 box'>category name</div>
-        <button className='category-box7 box'>category name</div>
-        <button className='category-box8 box'>category name</div> 
-        <button className='category-box9 box'>category name</div>       
+      
       </div>
       </div>
     );
