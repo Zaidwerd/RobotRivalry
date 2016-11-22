4183d93616bc3683a7d20576a08774b263fc6c9e
Andrew Diaz <andrew.matt.diaz@gmail.com>
2016-11-22 10:00:18 -0500

    add user models

---
+++ models/user.js
@@ -0,0 +1,76 @@
+/* eslint no-multi-spaces: ["error", { exceptions: { "VariableDeclarator": true } }] */
+/* eslint no-param-reassign: ["error", { "props": false }] */
+const pg = require('pg-promise')({/* OPTIONAL Initialization Options */});
+
+const config = {
+  host:     process.env.DB_HOST,
+  port:     process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASS,
+};
+
+const db = pg(config);
+const bcrypt       = require('bcryptjs');
+
+const SALTROUNDS = 10;
+
+module.exports = {
+
+  createUser(req, res, next) {
+  db.one(
+    username:
+    email:
+    first_name:
+    last_name:
+    age:
+    gender:
+    zodiac:
+    state:
+  )
+
+    // Store hashed password
+    password: bcrypt.hashSync(req.body.user.password, SALTROUNDS)
+  };
+
+  getDB().then((db) => {
+    db.collection('users')
+      .insert(userObject, (insertErr, dbUser) => {
+        if (insertErr) return next(insertErr);
+
+        res.user = dbUser;
+        db.close();
+        return next();
+      });
+  });
+}
+
+function getUserById(id) {
+  return getDB().then((db) => {
+    const promise = new Promise((resolve, reject) => {
+      db.collection('users')
+        .findOne({ _id: ObjectID(id) }, (findError, user) => {
+          if (findError) reject(findError);
+          db.close();
+          resolve(user);
+        });
+    });
+    return promise;
+  });
+}
+
+function getUserByUsername(username) {
+  return getDB().then((db) => {
+    const promise = new Promise((resolve, reject) => {
+      db.collection('users')
+        .findOne({ username }, (findError, user) => {
+          if (findError) reject(findError);
+          db.close();
+          resolve(user);
+        });
+    });
+    return promise;
+  });
+}
+
+};
