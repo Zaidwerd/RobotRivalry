4064caaebacce0d1eb20822b0755dfdc8d2c1977
grace moon <hi.gracemoon@gmail.com>
2016-11-22 13:41:13 -0500

    stats page

---
+++ src/components/Stats/Stats.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import App from './Stats.jsx';
+import style from './Stats.css';
+
+
+class Stats extends Component {
+
+  render() {
+    return (
+      <div id="container">
+
+        <div className="UserStats">
+        </div>
+
+        <div className="GlobalStats">
+        </div>
+        
+      </div>
+    )
+  }
+}
+export default Stats;
+
