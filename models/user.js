01e01c609f5886c5c99a792d132861e1c9d52bed
Andrew Diaz <andrew.matt.diaz@gmail.com>
2016-11-22 11:48:13 -0500

    add functions to user.js in models

--- models/user.js
+++ models/user.js
@@ -1,17 +1,7 @@
 /* eslint no-multi-spaces: ["error", { exceptions: { "VariableDeclarator": true } }] */
 /* eslint no-param-reassign: ["error", { "props": false }] */
-const pg = require('pg-promise')({/* OPTIONAL Initialization Options */});
-
-const config = {
-  host:     process.env.DB_HOST,
-  port:     process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user:     process.env.DB_USER,
-  password: process.env.DB_PASS,
-};
-
-const db = pg(config);
-const bcrypt       = require('bcryptjs');
+const db = require('../lib/dbConnect')
+const bcrypt = require('bcryptjs');
 
 const SALTROUNDS = 10;
 
@@ -19,58 +9,68 @@ module.exports = {
 
   createUser(req, res, next) {
   db.one(
-    username:
-    email:
-    first_name:
-    last_name:
-    age:
-    gender:
-    zodiac:
-    state:
+    'INSERT INTO users (username, email, first_name, last_name, age, gender, zodiac, state, password) Values ($/username/, $/email/, $/first_name/, $/last_name/, $/age/, $/gender/, $/zodiac/, $/state/, bcrypt.hashSync(req.body.user.password, SALTROUNDS));'
   )
-
     // Store hashed password
-    password: bcrypt.hashSync(req.body.user.password, SALTROUNDS)
+    // password: bcrypt.hashSync(req.body.user.password, SALTROUNDS)
   };
 
-  getDB().then((db) => {
-    db.collection('users')
-      .insert(userObject, (insertErr, dbUser) => {
-        if (insertErr) return next(insertErr);
-
-        res.user = dbUser;
-        db.close();
-        return next();
-      });
+  then((data) => {
+    res.rows = users;
+    next();
   });
+  .cathc(error => next(error));
 }
 
-function getUserById(id) {
-  return getDB().then((db) => {
-    const promise = new Promise((resolve, reject) => {
-      db.collection('users')
-        .findOne({ _id: ObjectID(id) }, (findError, user) => {
-          if (findError) reject(findError);
-          db.close();
-          resolve(user);
-        });
-    });
-    return promise;
+getUserById(id) {
+  const promise = new Promise((resolve, reject) => {
+  db.one(
+    'SELECT * FROM users Where id = $/id/')
+  .then((user) => {
+    res.rows = user;
+    next();
+  })
+  .catch(error => next(error));
   });
+  return promise;
+
+
+  // return getDB().then((db) => {
+  //   const promise = new Promise((resolve, reject) => {
+  //     db.collection('users')
+  //       .findOne({ _id: ObjectID(id) }, (findError, user) => {
+  //         if (findError) reject(findError);
+  //         db.close();
+  //         resolve(user);
+  //       });
+  //   });
+  //   return promise;
+  // });
 }
 
-function getUserByUsername(username) {
-  return getDB().then((db) => {
-    const promise = new Promise((resolve, reject) => {
-      db.collection('users')
-        .findOne({ username }, (findError, user) => {
-          if (findError) reject(findError);
-          db.close();
-          resolve(user);
-        });
-    });
-    return promise;
+getUserByUsername(username) {
+  const promise = new Promise((resolve, reject) => {
+    db.one(
+    'SELECT * FROM users Where username = $/username/')
+  .then((user) => {
+    res.rows = user;
+    next();
+  })
+  .catch(error => next(error));
   });
+  return promise;
+
+//   return getDB().then((db) => {
+//     const promise = new Promise((resolve, reject) => {
+//       db.collection('users')
+//         .findOne({ username }, (findError, user) => {
+//           if (findError) reject(findError);
+//           db.close();
+//           resolve(user);
+//         });
+//     });
+//     return promise;
+//   });
 }
 
 };
