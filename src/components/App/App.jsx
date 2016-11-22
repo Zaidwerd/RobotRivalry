de6a6333c89fea639f30575b8427a0f985ab0a1a
Scott McCord <scott.sdm27@gmail.com>
2016-11-22 09:57:07 -0500

    added GameState template

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import './normalize.css';
+import GameState from '../GameState/GameState.jsx';
 import style from './App.css';
+import './normalize.css';
 
 class App extends Component {
   render(){
     return (
       <div id="app-container">
-      <h1>hey</h1>
-        </div>
+        <h1>hey</h1>
+        <GameState />
+      </div>
     );
   }
 }
---
+++ src/components/GameState/GameState.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './GameState.css';
+
+class GameState extends Component {
+  render() {
+    return(
+      <container>
+        <h2> Scott hello </h2>
+      </container>
+      );
+  }
+}
+
+export default GameState;
