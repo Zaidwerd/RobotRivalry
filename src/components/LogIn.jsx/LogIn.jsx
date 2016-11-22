72ea6df9a711939d3271728c0da5a7487620cef3
grace moon <hi.gracemoon@gmail.com>
2016-11-22 10:51:17 -0500

    login page

---
+++ src/components/LogIn.jsx/LogIn.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import style from './LogIn.css';
+
+class LogIn extends Component {
+
+	render() {
+		return (
+			<div className="Container">
+				<input
+					type='text'
+					placeholder='Username'
+					value='#'
+				/>
+				<input='text'
+				placeholder='Password'
+				value='#'
+				/>
+				<button onClick='#'>
+					LOG IN
+				</button
+			</div>
+
+			)
+	}
+}
