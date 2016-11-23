636571151c2f141801608a5381bd713e3e4c243b
grace moon <hi.gracemoon@gmail.com>
2016-11-23 01:59:05 -0500

    bg image cover

--- src/components/App/StartPage/StartPage.jsx
+++ src/components/App/StartPage/StartPage.jsx
@@ -6,9 +6,13 @@ class StartPage extends Component {
   render(){
     return (
       <div className="container">
+
+      <div className="bg">
       <div className="robotrivalry">Robot Rivalry</div>
       <div className="cookingmama">cookingmama coming soon</div>
       <div className="pokemon">cookingmama coming soon</div>
+      </div>
+
       </div>
       );
   }
