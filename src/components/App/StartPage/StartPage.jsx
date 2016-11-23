b5971ef2eadda27891462deb5414c577685e35e2
grace moon <hi.gracemoon@gmail.com>
2016-11-23 17:34:29 -0500

    StartPage adding REACT Route Link to Cat page

--- src/components/App/StartPage/StartPage.jsx
+++ src/components/App/StartPage/StartPage.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './starterBG.jpg';
 import style from './StartPage.css';
+import { Link } from 'react-router';
+import Categories from './../Categories/categories.jsx';
+
 
 class StartPage extends Component {
   render(){
@@ -9,7 +12,10 @@ class StartPage extends Component {
       <div className="container">
 
       <div className="bg">
-      <div className="robotrivalry">Robot Rivalry</div>
+      <div className="robotrivalry">
+        <Link to="/categories">Go To Sample</Link>
+      </div>
+
       <div className="cookingmama">cookingmama coming soon</div>
       <div className="pokemon">pokemon coming soon</div>
       </div>
