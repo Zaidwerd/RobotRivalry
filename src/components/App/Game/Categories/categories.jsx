44d8a97647de6e1c437528e0dd84ff952b5ea26c
Debika Sarkar <debikasarkar713@gmail.com>
2016-11-25 11:39:51 -0500

    figuring out how to parse through categories

--- src/components/App/Game/Categories/categories.jsx
+++ src/components/App/Game/Categories/categories.jsx
@@ -10,12 +10,15 @@ class Categories extends Component {
       <div id='container'>
 
       <div className='main-box'>
-        <button
-          className='category-box1 box'
-          onClick={() => setState({
-            value: i
-          })
-        }>category name</button>
+        <button className='category-box1 box'>General Knowledge</button>
+        <button className='category-box2 box'>Celebrities</button>
+        <button className='category-box3 box'>Television</button>
+        <button className='category-box4 box'>History</button>
+        <button className='category-box5 box'>Politics</button>
+        <button className='category-box6 box'>Geography</button>
+        <button className='category-box7 box'>Animals</button>
+        <button className='category-box8 box'>Music</button>
+        <button className='category-box9 box'>Sports</button>
       </div>
 
       </div>
