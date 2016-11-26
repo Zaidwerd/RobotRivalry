c0e85d02e4116b791d65021ac1a40874ae899c50
grace moon <hi.gracemoon@gmail.com>
2016-11-26 12:09:07 -0500

    home page

--- src/components/App/Home.jsx
+++ src/components/App/Home.jsx
@@ -4,11 +4,17 @@ import style from './Home.css';
 class Home extends Component {
   render() {
     return (
-      <div>
-      <h1>Home</h1>
-      <a href="./#/login">Login</a>
-      <a href="./#/signup">Sign Up</a>
-      </div>
+      <div id="cont">
+      <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,700|Titillium+Web:200,300,400,400i,600,700,900" rel="stylesheet" />
+
+	      <div className="welcome-box">
+	      	<h1>WELCOME</h1>
+	      	<div className="links-area">
+			      <a className="welcome-button" href="./#/login">LOG IN</a><span> || </span><a className="welcome-button" href="./#/signup">SIGN UP</a>
+		      </div>
+	      </div>
+
+	    </div>  
       )
   }
 
