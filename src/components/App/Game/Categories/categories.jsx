24501837b9fd777a01cb8943200d5327294a1bc7
Debika Sarkar <debikasarkar713@gmail.com>
2016-11-26 18:47:10 -0500

    added event to buttons

--- src/components/App/Game/Categories/categories.jsx
+++ src/components/App/Game/Categories/categories.jsx
@@ -182,15 +182,15 @@ class Categories extends Component {
           </div>
           <div className="box-container">
             <div className='main-box'>
-              <button onClick={event => this.catMisc(event)}     className='category-box1 box'>General Knowledge</button>
-              <button onClick={event => this.catSong(event)}     className='category-box2 box'>Music</button>
-              <button onClick={event => this.catTele(event)}     className='category-box3 box'>Television</button>
-              <button onClick={event => this.catSports(event)}   className='category-box4 box'>Sports</button>
-              <button onClick={event => this.catGeo(event)}      className='category-box5 box'>Geography</button>
-              <button onClick={event => this.catHistory(event)}  className='category-box6 box'>History</button>
-              <button onClick={event => this.catPolitics(event)} className='category-box7 box'>Politics</button>
-              <button onClick={event => this.catCelebs(event)}   className='category-box8 box'>Celebrities</button>
-              <button onClick={event => this.catAnimals(event)}  className='category-box9 box'>Animals</button>
+              <button onClick={event => this.getKnow(event)}     className='category-box1 box'>General Knowledge</button>
+              <button onClick={event => this.getMusic(event)}    className='category-box2 box'>Music</button>
+              <button onClick={event => this.getTV(event)}       className='category-box3 box'>Television</button>
+              <button onClick={event => this.getSports(event)}   className='category-box4 box'>Sports</button>
+              <button onClick={event => this.getGeo(event)}      className='category-box5 box'>Geography</button>
+              <button onClick={event => this.getHistory(event)}  className='category-box6 box'>History</button>
+              <button onClick={event => this.getPolitics(event)} className='category-box7 box'>Politics</button>
+              <button onClick={event => this.getCelebs(event)}   className='category-box8 box'>Celebrities</button>
+              <button onClick={event => this.getAnimals(event)}  className='category-box9 box'>Animals</button>
             </div>
           </div>
 
