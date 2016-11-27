02c4d716c3e190aafa360c8fd3770e5967d2fce5
Debika Sarkar <debikasarkar713@gmail.com>
2016-11-27 13:15:37 -0500

    moved categories fetch calls to gamestate

--- src/components/App/Game/GameState/GameState.jsx
+++ src/components/App/Game/GameState/GameState.jsx
@@ -1,8 +1,140 @@
 import React, { Component } from 'react';
 import Question from '../Question/question';
-import './GameState.css';
+import style from './GameState.css';
 
 class GameState extends Component {
+  constructor(props){
+    super();
+  }
+
+  getKnow(){
+    console.log('General Knowledge');
+    fetch('/category', {
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        GeneralKnowledge: this.state.GeneralKnowledge
+      })
+    })
+    .catch(error => console.log(error));
+  }
+
+  getMusic(){
+    console.log('music');
+    fetch('/category', {
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        Music: this.state.Music
+      })
+    })
+    .catch(error => console.log(error));
+  }
+
+  getTV(){
+    console.log('tv');
+    fetch('/category', {
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        Television: this.state.Television
+      })
+    })
+    .catch(error => console.log(error));
+  }
+
+  getSports(){
+    console.log('sports');
+    fetch('/category', {
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        Sports: this.state.Sports
+      })
+    })
+    .catch(error => console.log(error));
+  }
+
+  getGeo(){
+    console.log('geography');
+    fetch('/category', {
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        Geography: this.state.Geography
+      })
+    })
+    .catch(error => console.log(error));
+  }
+
+  getHistory(){
+    console.log('history')
+    fetch('/category', {
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        History: this.state.History
+      })
+    })
+    .catch(error => console.log(error));
+  }
+
+  getPolitics(){
+    console.log('politics');
+    fetch('/category', {
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        Politics: this.state.Politics
+      })
+    })
+    .catch(error => console.log(error));
+  }
+
+  getCelebs(){
+    console.log('celebrities');
+    fetch('/category', {
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        Celebs: this.state.Celebs
+      })
+    })
+    .catch(error => console.log(error));
+  }
+
+  getAnimals(){
+    console.log('animals');
+    fetch('/category', {
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        Animals: this.state.Animals
+      })
+    })
+    .catch(error => console.log(error));
+  }
+
+
+
   render() {
 
 
@@ -20,6 +152,32 @@ class GameState extends Component {
         <button onClick={this.props.nextQuestionD}>{this.props.answerD}</button>
 
       </container>
+      <div id='container'>
+      <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,700|Titillium+Web:200,300,400,400i,600,700,900" rel="stylesheet" />
+
+
+        <div className="ugh">
+
+          <div className='hOne'>
+            <h1 className="catH1">CHOOSE A CATEGORY</h1>
+          </div>
+          <div className="box-container">
+            <div className='main-box'>
+              <button onClick={event => this.getKnow(event)}     className='category-box1 box'>General Knowledge</button>
+              <button onClick={event => this.getMusic(event)}    className='category-box2 box'>Music</button>
+              <button onClick={event => this.getTV(event)}       className='category-box3 box'>Television</button>
+              <button onClick={event => this.getSports(event)}   className='category-box4 box'>Sports</button>
+              <button onClick={event => this.getGeo(event)}      className='category-box5 box'>Geography</button>
+              <button onClick={event => this.getHistory(event)}  className='category-box6 box'>History</button>
+              <button onClick={event => this.getPolitics(event)} className='category-box7 box'>Politics</button>
+              <button onClick={event => this.getCelebs(event)}   className='category-box8 box'>Celebrities</button>
+              <button onClick={event => this.getAnimals(event)}  className='category-box9 box'>Animals</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
       );
   }
 }
